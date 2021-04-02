e47ef1df4a9279c7103d18e8716ca24868e41dc5
Ash Holland <ash@sorrel.sh>
2021-04-02 23:44:22 +0100

    Fix Fish mkvenv with implicit path

--- private_dot_config/private_fish/functions/mkvenv.fish
+++ private_dot_config/private_fish/functions/mkvenv.fish
@@ -1,6 +1,7 @@
 function mkvenv
     set python $argv[1]
     set path $argv[2]
+    test -n "$path"; or set path venv
     set argv $argv[3..]
     $python -m venv $path $argv
     # TODO: with Python 3.8+, pass `--upgrade-deps` above instead of running this.
